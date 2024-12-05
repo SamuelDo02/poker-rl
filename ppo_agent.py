20d3f8930e630b392c76be8c73dc35ef343875a9
Samuel Do <dosamuel02@gmail.com>
2024-12-05 14:13:06 -0800

    feat: add ppo agent file

---
+++ ppo_agent.py
@@ -0,0 +1,28 @@
+class PPOAgent:
+    def __init__(self):
+      pass
+
+
+    def step(state):
+        ''' Predict the action given the curent state in generating training data.
+
+        Args:
+            state (dict): An dictionary that represents the current state
+
+        Returns:
+            action (int): The action predicted by the agent.
+        '''
+        pass
+
+
+    def eval_step(self, state):
+        ''' Predict the action given the current state for evaluation.
+
+        Args:
+            state (dict): An dictionary that represents the current state
+
+        Returns:
+            action (int): The action predicted predicted by the agent.
+            probs (list): The list of action probabilities.
+        '''
+        pass
