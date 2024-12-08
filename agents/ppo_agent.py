56afecf4ef93bdd46f86240cac8f69d2a8884fdc
deniselj24 <122917446+deniselj24@users.noreply.github.com>
2024-12-08 15:19:21 -0800

    Update ppo_agent.py

--- agents/ppo_agent.py
+++ agents/ppo_agent.py
@@ -109,10 +109,9 @@ class PPOAgent:
             # make a new dim for num_actors to do rollouts in parallel
             actors_states = states.reshape([states.shape[0], self.num_actors, 1])
             # rollout current policy in environment for T timesteps
-            cur_log_probs, prev_log_probs, advantages, states = self.rollout(actors_states, self.gamma, self.num_timesteps)
+            cur_log_probs, prev_log_probs, advantages, states = self.rollout(actors_states, self.num_timesteps)
             ratios = torch.exp(cur_log_probs - prev_log_probs)
             surrogate_loss = self.policy.compute_surrogate_loss(ratios, advantages, self.epsilon)
             for k in self.num_epochs:
                 surrogate_loss.backward()
             self.old_policy = self.policy
-        
