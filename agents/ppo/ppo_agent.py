7b958e27fb9b5418a6efc834cff1dc94312fb281
deniselj24 <122917446+deniselj24@users.noreply.github.com>
2024-12-08 17:11:07 -0800

    Update ppo_agent.py

--- agents/ppo/ppo_agent.py
+++ agents/ppo/ppo_agent.py
@@ -1,3 +1,5 @@
+import torch
+
 class PPOAgent:
     def __init__(self): 
         self.use_raw = False
@@ -5,7 +7,7 @@ class PPOAgent:
         self.value_f = ValueEstimator()
         self.gamma = 0.99 # random value
 
-    def step(state):
+    def step(self, state):
         ''' Predict the action given the current state in generating training data.
 
         Args:
@@ -14,7 +16,13 @@ class PPOAgent:
         Returns:
             action (int): The action predicted by the agent.
         '''
-        pass
+        action_probs = self.policy(state['obs'])
+        action = None
+        while action not in list(state['legal_actions'].keys()):
+            # keep resampling
+            # action is an int / index
+            action = torch.multinomial(action_probs, num_samples=1, replacement=True)
+        return action
 
     def eval_step(self, state):
         ''' Predict the action given the current state for evaluation.
@@ -23,11 +31,26 @@ class PPOAgent:
             state (dict): An dictionary that represents the current state
 
         Returns:
-            action (int): The action predicted predicted by the agent.
+            action (int): The action predicted by the agent.
             probs (list): The list of action probabilities.
         '''
-        print(state)
-        pass
+        action_probs = self.policy(state['obs'])
+        action = None
+        while action not in list(state['legal_actions'].keys()):
+            # keep resampling
+            # action is an int / index
+            action = torch.multinomial(action_probs, num_samples=1, replacement=True)
+
+        mask = torch.zeros_like(action_probs)
+        mask[state['legal_actions'].keys()] = 1.0
+        masked_probs = action_probs * mask
+        total = masked_probs.sum()
+        rescaled_action_probs = masked_probs / total
+        # action = torch.multinomial(rescaled_action_probs, num_samples=1, replacement=True)
+
+        info = {}
+        info['probs'] = {state['raw_legal_actions'][i]: rescaled_action_probs[list(state['legal_actions'].keys())[i]] for i in range(len(state['legal_actions']))}
+        return action, info
 
     def advantage(self):
         pass
